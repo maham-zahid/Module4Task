6e9f77b5cbec444df814a351c7f3ecd24e73f581
maham-zahid <mahamzahid333@gmail.com>
2024-07-29 14:46:12 +0500

    custom hierarchical taxonomy Book Category

--- app/public/wp-content/plugins/wp-book/admin/class-wp-book-admin.php
+++ app/public/wp-content/plugins/wp-book/admin/class-wp-book-admin.php
@@ -117,4 +117,60 @@ class Wp_Book_Admin {
 		);
 	}
 
+
+
+
+	function custom_post_type() {
+  
+		// Set UI labels for Custom Post Type
+			$labels = array(
+				'name'                => _x( 'Books', 'Post Type General Name', 'twentythirteen' ),
+				'singular_name'       => _x( 'Book', 'Post Type Singular Name', 'twentythirteen' ),
+				'menu_name'           => __( 'Books', 'twentythirteen' ),
+				'parent_item_colon'   => __( 'Parent Book', 'twentythirteen' ),
+				'all_items'           => __( 'All Books', 'twentythirteen' ),
+				'view_item'           => __( 'View Book', 'twentythirteen' ),
+				'add_new_item'        => __( 'Add New Book', 'twentythirteen' ),
+				'add_new'             => __( 'Add New', 'twentythirteen' ),
+				'edit_item'           => __( 'Edit Book', 'twentythirteen' ),
+				'update_item'         => __( 'Update Book', 'twentythirteen' ),
+				'search_items'        => __( 'Search Book', 'twentythirteen' ),
+				'not_found'           => __( 'Not Found', 'twentythirteen' ),
+				'not_found_in_trash'  => __( 'Not found in Trash', 'twentythirteen' ),
+			);
+			  
+		// Set other options for Custom Post Type
+			  
+			$args = array(
+				'label'               => __( 'books', 'twentythirteen' ),
+				'description'         => __( 'MBook news and reviews', 'twentythirteen' ),
+				'labels'              => $labels,
+				'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+				'hierarchical'        => false,
+				'public'              => true,
+				'show_ui'             => true,
+				'show_in_menu'        => true,
+				'show_in_nav_menus'   => true,
+				'show_in_admin_bar'   => true,
+				'menu_position'       => 5,
+				'can_export'          => true,
+				'has_archive'         => true,
+				'exclude_from_search' => false,
+				'publicly_queryable'  => true,
+				'capability_type'     => 'page',
+				'show_in_rest'        => true,
+				  
+				// This is where we add taxonomies to our CPT
+				'taxonomies'          => array( 'category' ),
+			);
+			  
+			// Registering your Custom Post Type
+			register_post_type( 'books', $args );
+		  
+		}
+
+
+
+
+
 }
--- app/public/wp-content/plugins/wp-book/includes/class-wp-book.php
+++ app/public/wp-content/plugins/wp-book/includes/class-wp-book.php
@@ -157,6 +157,7 @@ class Wp_Book {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 		$this->loader->add_action( 'init', $plugin_admin, 'register_book_post_type' );
+		$this->loader->add_action( 'init', $plugin_admin,'custom_post_type', 0 );
 
 	}
 
